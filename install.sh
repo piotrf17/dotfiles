5488bb99c748e4354aa2e151711813bbfe761a0f
Piotr Fidkowski <piotr@fidkowski.com>
2013-05-04 20:33:51 -0700

    added an installation script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Use gnu stow to install our config packages.  Any existing conflicting
+# config files are backed up.
+
+# Target installation directory for config packages.
+TARGET=${HOME}
+
+# Default config packages to install.
+DEFAULT="bash emacs git screen vim"
+
+# First, backup any conflicting files.
+for package in ${DEFAULT}
+do
+  for path in `stow -t ${TARGET} -c ${package} 2>&1 | awk '{print $4}'`
+  do
+    # Extract root file or directory, i.e from ${HOME}/foo/bar, return foo.
+    # There must be a nicer way to do this...
+    root=`echo ${path} | sed 's|${TARGET}/||' | awk '{split($1, a, "/"); print a[1]}'`
+    path=${HOME}/${root}
+    echo "Backing up existing file/directory: "${path}
+    mv ${path} ${path}.original
+  done
+done
+
+# Now, stow our default packages.
+for package in ${DEFAULT}
+do
+  stow -t ${TARGET} ${package}
+done
